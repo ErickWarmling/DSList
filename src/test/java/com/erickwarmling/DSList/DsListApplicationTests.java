e1789147c39ab9c5b64a6449825560ccf067e1e2
ErickWarmling <warmling.erick@gmail.com>
2025-01-22 23:26:48 -0300

    Project created

---
+++ src/test/java/com/erickwarmling/DSList/DsListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erickwarmling.DSList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
